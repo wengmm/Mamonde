d30209df4362286cd345c61ff1e135b963e2ad22
wengmm <573233142@qq.com>
2019-01-10 14:25:58 +0800

    fead pay

--- src/js/car.js
+++ src/js/car.js
@@ -4,7 +4,8 @@ require(["./requirejs.config"],function(){
 			
 		
 		var arr=JSON.parse($.cookie("cart"));
-				console.log(arr);
+			//	console.log(arr);
+			//	console.log($.cookie("cart"));
 				 //splice
 				 //将cookie内容渲染到tbody里面
 				var str="";
@@ -26,8 +27,21 @@ require(["./requirejs.config"],function(){
 					'</tr>';
 					
 				}
+				if(str==""){
+					alert("你的购物车比我的荷包还空哦！");
+					str='<tr><td colspan="6">购物车空空如也~</td></tr>'
+				};
 				$("tbody").html(str);
 				//console.log($(".dj").text());
+				function bjCok(){
+					for(var h=0;h<arr.length;h++){
+						arr[h].num=$("tbody tr").eq(h).find("._num").text()*1;
+						
+					};
+					$.cookie("cart",JSON.stringify(arr),{path:"/"})
+					
+				};
+				
 				fun();
 				function fun(){
 				for(var j=0;j<arr.length;j++){  //每个结算金额
@@ -48,6 +62,10 @@ require(["./requirejs.config"],function(){
 					$tr.find("span").text($tr.find(".number").val());
 					$tr.removeClass("edit");
 					fun();
+					_sum();
+					bjCok();
+					console.log($.cookie("cart"));
+					
 				});
 				$(".qx").on("click",function(){
 					let $tr=$(this).parent().parent();
@@ -55,87 +73,61 @@ require(["./requirejs.config"],function(){
 				});
 				$(".sc").on("click",function(){
 					let $tr=$(this).parent().parent();
+					let m=$tr.index();  //找到下标删除对应arr值再存cookie
+					//	console.log(m);
 					if(confirm("确定删除吗")){
 						$tr.remove();
-					}										
+						arr.splice(m,1);
+						$.cookie("cart",JSON.stringify(arr),{path:"/"});
+					}	
+					_sum();
+					
 				});
 				$("#allcheck").on("click",function(){
 					$(".check").prop("checked",true);//注意Pro与attr（一次）的区别
 					//count=target.checked? check.length:0;
+					_sum();
 					
 				});
 				$(".check").on("click",function(){
 					//console.log($(".check:checked").length);//选中数量
 					let $tr=$(this).parent().parent();
 					$(".check").length===$(".check:checked").length?	//jQuery的length是属性
-					$("#allcheck").prop("checked",true):$("#allcheck").prop("checked",false)
-					if($(".check:checked")){
-						$("#sum").text($tr.find(".je").text()*1+$("#sum").text()*1)
-					}
+					$("#allcheck").prop("checked",true):$("#allcheck").prop("checked",false);					
+					_sum();
 					
 				});
-				/*function fun(){
-					var sum=0;
-					
-				}*/
-				/*if(target.className==="bj"){
-						tr.className="edit";
-						var span=tools.$("span",tr);
-						//console.log(span)
-						for(var i=0;i<span.length;i++){
-							span[i].nextElementSibling.value=span[i].innerHTML;
-						}
-						}else if(target.className==="qd"){
-							tr.className="";
-							var span=tools.$("span",tr);
-							for(var i=0;i<span.length;i++){
-								span[i].innerHTML=span[i].nextElementSibling.value								
-							}
-							fun();
-						}else if(target.className==="qx"){
-							tr.className="";							
-						}else if(target.className==="sc"){
-							if(confirm("确定删除吗")){
-							tbody.removeChild(target.parentNode.parentNode);
-							var acheck=tools.$(".check",tr)[0];
-							if(acheck.checked){count--};
-							console.log(target.parentNode.parentNode);
-							//allcheck.checked=(count===check.length);
-						//	arr.splice(,1)
-							//tools.cookie("cart",arr[1],{expires:-1})
-							
-							fun();
-							}
-						}else if(target.id==="allcheck"){		//注意这里是id							
-							for(var j=0;j<check.length;j++){
-								check[j].checked=target.checked;
-								
-							}
-							count=(target.checked? check.length:0);
-							fun();
-						}else if(target.className==="check"){
-							target.checked? count++:count--;															
-							if(count===check.length){
-								allcheck.checked=true;
-							}else{allcheck.checked=false;}
-							
-							fun();
-						}
-												
-					}
 				
-				function fun(){		
+				function _sum(){//下面总的全部结算金额
 					var sum=0;
-					var atr=tools.$("tr",tbody);					
-					for(var k=0;k<atr.length;k++){
-						var span=tools.$("span",atr[k]);
-						if(tools.$(".check",atr[k])[0].checked){
-							 sum+=Number(span[1].innerHTML)*Number(span[2].innerHTML);
+					for(let k=0;k<$("tbody tr").length;k++){
+						//console.log($("tbody tr").length);						
+						if($("tbody tr").eq(k).find(".check").prop("checked")){
+							//console.log($("tbody tr").eq(k).find(".dj").text());
+							//console.log($("tbody tr").eq(k).find("._num").text());
+							sum+=$("tbody tr").eq(k).find(".dj").text()*1*($("tbody tr").eq(k).find("._num").text()*1)
 							
 						}
-						tools.$("#money").innerHTML=sum;
-					}*/
-					
+						
+					}
+					$("#sum").text(sum.toFixed(2));
+				};
+				
+				
+				//结算所选按钮存另一个数组 再存另一个cookie
+				
+				$("#oder").on("click",function(){
+					//$.removeCookie("aCar",{path: "/"});
+					var newArr=[];
+					for(let p=0;p<$(".check").length;p++){
+						if($(".check").eq(p).prop("checked")){
+							newArr.push(arr[p]);							
+						}						
+					}					
+					$.cookie("aCar",JSON.stringify(newArr),{path:"/"});
+					console.log($.cookie("aCar"));
+				})
+				
 				
 				
 	})			
---
+++ src/js/pay.js
@@ -0,0 +1,7 @@
+require(["./requirejs.config"],function(){
+	require(["jquery","header","footer","cookie"],function(){})
+	
+
+
+
+})
